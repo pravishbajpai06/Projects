5114fe7a5a9cfb99dd5eb3844e6e442e558dbb15
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 10:05:24 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,7 @@
+#2-Find e to the Nth Digit - Just like the previous problem, but with e instead of π (pi). Enter a number and have the program generate e up to that many decimal
+#places. Keep a limit to how far the program will go.
 
+
+from math import e
+x=int(input())
+print(round(e,x))
