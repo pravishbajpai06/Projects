644f484b199fd866aaca98acd5fa97a1b1c268e2
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 10:16:09 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,1 +1,18 @@
+#11-Tax Calculator - Asks the user to enter a cost and either a country or state tax. It then returns the tax plus the total cost with tax.
 
+
+def statetax(number):
+    s=float(input("Enter your state tax value"))
+    st=s*number
+    return st
+def countrytax(number):
+    c=float(input("Enter your country tax"))
+    ct=c*number
+    return ct
+def totaltax(number):
+    country=countrytax(number)
+    state=statetax(number)
+    totaltax=country+state
+    return totaltax
+n=float(input("Enter your cost"))
+print(totaltax(n))
