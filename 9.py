034a588ff6b6cfeb64c9f99f77b662a598a032fb
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 10:14:21 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,1 +1,14 @@
+#13-Distance Between Two Cities - Calculates the distance between two cities and allows the user to specify a unit of distance. This program may require finding coordinates for the cities like latitude and longitude.
 
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+geolocator = Nominatim(user_agent="http")
+loc1=input("enter your first city")
+loc2=input("enter your second city")
+location1 = geolocator.geocode(loc1)
+location2=geolocator.geocode(loc2)
+a=(location1.latitude,location1.longitude)
+b=(location2.latitude,location2.longitude)
+print(a)
+print(b)
+print(geodesic(a,b).miles)
