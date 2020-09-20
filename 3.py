d71cf0c23339871eec7a8050c34f01cd2a4d27a8
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 10:06:11 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,13 @@
+#3-Fibonacci Sequence - Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
 
+def fib(n):
+    if n<=1:
+        return n
+    else:
+        return(fib(n-1)+fib(n-2))
+n=int(input())
+if n<=0:
+    print("enter positive integer")
+else:
+    for i in range(1,n+1):
+        print(fib(i))
