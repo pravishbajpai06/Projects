7a8259871fd805fefcd79e09e36551f49091ecf0
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 09:56:17 +0530

    Update PI.py

--- PI.py
+++ PI.py
@@ -1,1 +1,7 @@
 
+#1-Find PI to the Nth Digit - Enter a number and have the program generate π (pi) up to that many decimal places. Keep a limit to how far the program will go.
+
+
+from math import pi
+x=int(input())
+print("{:.{}f}".format(pi, x))
