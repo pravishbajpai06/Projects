cc7b0ea010a2efbfe0c414f429983495c3d80f83
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 11:53:48 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,1 +1,14 @@
-
+#20-Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of tails and heads.
+import random
+n=int(input("How many tims do you want to flip the coin"))
+h=0
+t=0
+for i in range(1,n+1):
+    flip=random.choice(["h","t"])
+    print(flip)
+    if flip=="h":
+        h+=1
+    elif flip=="t":
+        t+=1
+print("no. of times tails came is",t)
+print("no. of time heads came is",h)
