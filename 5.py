1317fcc7c02f1a0789bb253edf9dc8cc51b57306
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-09-20 10:07:52 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,23 @@
+#5- Next Prime Number - Have the program find prime numbers until the user chooses to stop asking for the next one.
+n=int(input())
+i=1
+l=[]
+for k in range(1,n+1):
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c=c+1
+        
+    if c==2:
+        l.append(i)
+        
+    i=i+1        
+print(l)
+for i in range(n):
+    print("do you want to get the next prime number:  Enter y or n")
+    a=input()
+    if a=="y":
+        print (l[i])
+    elif a=="n":
+        break
 
